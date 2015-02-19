c1f6c185355d7aef3bdbf976947871d5121452a5
brunetto <brunetto.ziosi@gmail.com>
2015-02-19 15:28:27 +0100

    init

---
+++ veuszImportJSON.py
@@ -0,0 +1,38 @@
+import json
+from veusz.plugins import *
+
+class ImportJSON(ImportPlugin):
+    """
+    Read data from JSON file
+    """
+
+    name = "LoadJSON"
+    author = "Brunetto Ziosi <brunetto.ziosi@gmail.com>"
+    description = "Reads data form a JSON file"
+
+    # Uncomment this line for the plugin to get its own tab
+    promote_tab='LoadJSON'
+
+    file_extensions = set(['.json'])
+
+    def __init__(self):
+        ImportPlugin.__init__(self)
+        self.fields = [
+            ImportFieldText("name", descr="Dataset name", default="name"),
+            ]
+
+    def doImport(self, params):
+        """Actually import data
+        params is a ImportPluginParams object.
+        Return a list of ImportDataset1D, ImportDataset2D objects
+        """
+        f = params.openFileWithEncoding()
+        data = []
+        tmp = json.load(f)
+        for name, value in iter(sorted(tmp.iteritems())):
+            data.append(ImportDataset1D(name, value))
+            print name, len(value), len(data)
+        return data
+
+# add the class to the registry. An instance also works, but is deprecated
+importpluginregistry.append(ImportPluginExample)
